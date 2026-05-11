c81159ddbb34babccd7d05fb5e504c97bb886a4d
fisayo-dev <olufisayobadina@gmail.com>
2026-05-11 15:19:13 +0100

    style: ehance styling

--- web/components/home/footer.tsx
+++ web/components/home/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="app-container ">
       <div className="grid gap-6 mt-14 py-16 border-t border-gray/50">
         <div className="grid md:flex items-center justify-between gap-10">
-          <h2 className="text-4xl md:text-9xl">VerifyNG</h2>
+          <h2 className="text-5xl md:text-9xl font-bold">VerifyNG</h2>
           <div className="flex flex-col">
             <p className="text-xl font-bold mb-4">Built for Squad Hackathon 3.0</p>
             <p>Fisayo Obadina</p>
