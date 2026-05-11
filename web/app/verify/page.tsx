7b41c5df7220a347fec4616e5bd6b2b21e038114
fisayo-dev <olufisayobadina@gmail.com>
2026-05-11 17:24:22 +0100

    feat: add verify document button

--- web/app/verify/page.tsx
+++ web/app/verify/page.tsx
@@ -1,10 +1,11 @@
 import { DocumentIcon } from "@heroicons/react/24/outline";
 import VerifyUploadDropzone from "./verify-upload-dropzone";
+import Button from "@/components/general/button";
 
 const VerifyPage = () => {
   return (
     <div className="w-full h-100 place-content-center text-center mb-20">
-      <div className="app-container grid gap-6">
+      <div className="app-container grid gap-6  pb-20">
         <div className="flex flex-col gap-3">
           <h2 className="text-4xl font-extrabold">Upload Certificate</h2>
           <p className="text-sm px-5 py-2 rounded-full bg-gray/20 w-fit mx-auto flex items-center gap-2">
@@ -13,6 +14,9 @@ const VerifyPage = () => {
           </p>
         </div>
         <VerifyUploadDropzone />
+        <div className="mx-auto">
+          <Button>Verify Document</Button>
+        </div>
       </div>
     </div>
   );
