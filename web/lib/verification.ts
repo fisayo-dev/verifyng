48f314d753e2e27b1628c3e8d4949650957a0edc
fisayo-dev <olufisayobadina@gmail.com>
2026-05-14 00:22:06 +0100

    feat: create server action file for verify document

--- web/lib/verification.ts
+++ web/lib/verification.ts
@@ -1,5 +1,6 @@
 import api from "@/lib/axios";
 import type { VerificationResult } from "@/types/verification";
+import { redirect } from "next/navigation";
 
 export const getVerificationResult = async (
   jobId: string,
@@ -9,4 +10,23 @@ export const getVerificationResult = async (
   );
 
   return response.data;
+};
+
+export const verifyDocument = async (file_data: File) => {
+  try {
+    const formData = new FormData();
+    formData.append("file", file_data);
+
+    const response = await api.post("/verify", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    const { checkout_url } = response.data;
+
+    redirect(checkout_url);
+  } catch {
+    console.error("Verfification failed");
+  }
 };
