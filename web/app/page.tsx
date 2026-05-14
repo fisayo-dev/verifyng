d4648bd668f7f4b45feeb293590c5e14aed2cc77
fisayo-dev <olufisayobadina@gmail.com>
2026-05-15 00:18:21 +0100

    feat: ship steps component

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,13 @@
 import Features from "@/components/home/features";
 import Footer from "@/components/home/footer";
 import Hero from "@/components/home/hero";
+import Step from "@/components/home/step";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <Step />
       <Features />
       <Footer />
     </div>
