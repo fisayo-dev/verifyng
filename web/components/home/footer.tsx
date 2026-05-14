9cb5c0923899307be4c69a5095d083769fa960eb
fisayo-dev <olufisayobadina@gmail.com>
2026-05-14 08:18:19 +0100

    style: tweak top border

--- web/components/home/footer.tsx
+++ web/components/home/footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
   return (
     <div className="app-container ">
-      <div className="grid gap-6 mt-14 py-16 border-t border-gray/50">
+      <div className="grid gap-6 mt-14 py-16 border-t border-gray/20">
         <div className="grid md:flex items-center justify-between gap-10">
           <h2 className="text-5xl md:text-9xl font-bold">VerifyNG</h2>
           <div className="flex flex-col">
-            <p className="text-xl font-bold mb-4">Built for Squad Hackathon 3.0</p>
+            <p className="text-xl font-bold mb-4">
+              Built for Squad Hackathon 3.0
+            </p>
             <p>Fisayo Obadina</p>
             <p>Divine Aghulor</p>
             <p>Olatunji Oluwatobiloba</p>
